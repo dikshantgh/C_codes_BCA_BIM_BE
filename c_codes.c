275f0493cb7030229c7f403e7935755095791079
Dikshant Ghimire <dikkughimire@gmail.com>
2022-02-01 14:52:47 +0545

    Update c_codes.c

--- c_codes.c
+++ c_codes.c
@@ -1,1 +1,36 @@
-
+//it can also be done using sort then printf("%d", array[1])
+//second largest
+#include<stdio.h>
+#include<limits.h>
+ 
+int main()
+{
+  int n, array[20];
+ 
+  printf("Enter the size: ");
+  scanf("%d",&n);
+ 
+  printf("Enter the elements: ");
+  for(int i=0; i<n; i++){
+    scanf("%d",&array[i]);
+  }
+  
+//   12_   10_       11__
+//   max max_less
+  int max = INT_MIN;
+  int secondmax = INT_MIN;
+  
+  for(int i=0; i<n; i++){
+    if(array[i]>max){
+      secondmax = max;
+      max = array[i];
+    }
+    else if(array[i]<max && array[i]>secondmax){
+      secondmax = array[i];
+    }
+  }
+ 
+  printf("Second largest element is %d",secondmax);
+ 
+  return 0;
+}
