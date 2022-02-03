e1b33ef8597ba969cb523503cc49c2580e17f8e0
Dikshant Ghimire <dikkughimire@gmail.com>
2022-02-03 19:22:34 +0545

    Update c_codes.c

--- c_codes.c
+++ c_codes.c
@@ -1,3 +1,6 @@
+
+// One Dimensional Array
+
 //it can also be done using sort then printf("%d", array[1])
 //second largest
 #include<stdio.h>
@@ -15,8 +18,6 @@ int main()
     scanf("%d",&array[i]);
   }
   
-//   12_   10_       11__
-//   max max_less
   int max = INT_MIN;
   int secondmax = INT_MIN;
   
@@ -34,3 +35,63 @@ int main()
  
   return 0;
 }
+
+
+
+//WAP in c to find the maximum and minimum from the given 1D integer array
+
+
+
+
+
+// Multidimensional Array
+
+// add two 2D array
+// sum of all the elements of two 3D array
+//2D array multiplication
+//2D matrix diagonal elements sum
+//sum of all the elements of 2D array(2D matrix)
+//sum of all the elements of two 2D matrix
+#include <stdio.h>
+
+int main() {
+    
+    
+    puts("Enter the row and column size of a mtrix");
+    int row,col,sum=0;
+    scanf("%d%d",&row,&col);
+    int a[row][col];
+    
+    printf("Enter the elements of array for size %d X %d",row,col);
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            printf("\na[%d][%d]:",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+    puts("DIsplaying the given matrix");
+     for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            sum = sum+a[i][j];
+        }
+    }
+    
+    printf("The sum of all the elements of the given array is %d",sum);
+    return 0;
+}
+
+
