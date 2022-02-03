b3db780d4f57ca5ef30fb24d5edb2e12d5149b5e
Dikshant Ghimire <dikkughimire@gmail.com>
2022-02-03 19:47:52 +0545

    Update c_codes.c

--- c_codes.c
+++ c_codes.c
@@ -143,4 +143,40 @@ int main() {
 
 
 
+//primary and seconday diagonal sum (next method)
+#include <stdio.h>
 
+int main() {
+    int row=3,col=3,sum=0, sum_s=0;
+    int a[row][col];
+   
+    
+    printf("Enter the elements of first array for size %d X %d",row,col);
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            printf("\na[%d][%d]:",i,j);
+            scanf("%d",&a[i][j]);
+        }
+    }
+    
+    puts("DIsplaying the given matrix1");
+     for(int i=0;i<row;i++)
+    {
+        for(int j=0;j<col;j++)
+        {
+            printf("%d\t",a[i][j]);
+        }
+        printf("\n");
+    }
+    
+    
+    for(int i=0;i<row;i++)
+    {
+                sum = sum+a[i][i];
+                sum_s = sum_s+a[i][row-i-1];
+    }
+  printf("THe sum of primary diagonal is %d \nThe sum of secondary diagonal is %d",sum,sum_s);
+    return 0;
+}
