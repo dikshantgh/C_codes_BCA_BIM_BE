4f9087cc5af6a561bb5a19612fa7c3fc65e3e422
Dikshant Ghimire <dikkughimire@gmail.com>
2022-02-03 21:46:43 +0545

    Update c_codes.c

--- c_codes.c
+++ c_codes.c
@@ -376,3 +376,52 @@ int main() {
 }
 
 
+//WAP in C to add two distances given in feet and inch using //structure.
+#include <stdio.h>
+
+struct Distance
+{
+    float inch;
+    int feet;
+}d[2],result;
+
+
+int main() {
+    for(int i=0;i<2;i++)
+    {
+        printf("Enter the details for distance_%d as follows\n",i);
+       
+        puts("Enter the feet:");
+        scanf("%d",&d[i].feet);
+        puts("Enter the inch:");
+        scanf("%f",&d[i].inch);
+    }
+    //display record
+    puts("\nDisplaying all records\n");
+    for(int i=0;i<2;i++)
+    {
+        printf("\nDistance_%d\n",i);
+        printf("inch: %.2f\nfeet: %d\n",d[i].inch,d[i].feet);
+    }
+    
+    int j=0;
+    for(int i=0;i<2;i++)
+    {
+        result.inch = d[i].inch + d[j].inch; 
+        result.feet = d[i].feet + d[j].feet;
+        j++;
+    }
+    while(result.inch>=12)
+    {
+        result.inch = result.inch-12;
+        result.feet++;
+    }
+    
+    printf("\nThe sum of both the distance is %d feet and %.2f inches",result.inch, result.feet);
+    
+    return 0;
+}
+
+
+
+
